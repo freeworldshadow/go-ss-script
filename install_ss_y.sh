bb3cac228a2b0a6fc35d2fa4d4dfc8c6ed66adf2
freeworldshadow <97747880+freeworldshadow@users.noreply.github.com>
2025-04-10 11:02:35 +0800

    Update install_ss_y.sh

--- install_ss_y.sh
+++ install_ss_y.sh
@@ -14,7 +14,7 @@ set -e
 echo "🔧 开始安装 Shadowsocks（适配 ARM64）"
 
 # === 更新系统 ===
-sudo apt update && sudo apt upgrade -y
+sudo -E apt update && sudo -E apt upgrade -y
 sudo apt install -y curl git
 
 # === 检测系统架构 ===
